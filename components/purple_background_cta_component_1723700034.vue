a79ba2f7da1f776027d76406ae1f761ab3d7ddb6
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-12 13:34:02 -0400

    asdfasfdsadf

---
+++ components/purple_background_cta_component_1723700034.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">Change Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello from dynamic SFC!',
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = 'Title Changed!';
+    },
+  },
+};
+</script>
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <SimpleHeaderComponent />
+    <PurpleHeroComponent />
+    <PurpleHeaderComponent />
+    <PurpleFeatureGridComponent />
+    <PurpleBackgroundCtaComponent />
+    <ParticleComponent />
+    <DarkFooterComponent />
+  </div>
+</template>
+
+<script>
+import SimpleHeaderComponent from '../components/simple_header_component_1724462188.vue';
+import PurpleHeroComponent from '../components/purple_hero_component_1723700034.vue';
+import PurpleHeaderComponent from '../components/purple_header_component_1723700034.vue';
+import PurpleFeatureGridComponent from '../components/purple_feature_grid_component_1723700034.vue';
+import PurpleBackgroundCtaComponent from '../components/purple_background_cta_component_1723700034.vue';
+import ParticleComponent from '../components/particle_component_1723700034.vue';
+import DarkFooterComponent from '../components/dark_footer_component_1723700034.vue';
+
+export default {
+  components: {
+    SimpleHeaderComponent,
+    PurpleHeroComponent,
+    PurpleHeaderComponent,
+    PurpleFeatureGridComponent,
+    PurpleBackgroundCtaComponent,
+    ParticleComponent,
+    DarkFooterComponent,
+  },
+};
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
